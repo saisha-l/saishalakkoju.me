b70c1efdac5c2dca118ded614902db7ba102ee16
Saisha <87923617+saisha-l@users.noreply.github.com>
2024-11-13 13:37:01 -0800

    Update package files and index.js

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,28 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import React from 'react';
+import { TypeAnimation } from 'react-type-animation';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+      
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        <TypeAnimation
+          sequence={['Saisha Lakkoju']}
+          wrapper="div"
+          cursor={true}
+          style={{ fontSize: '3em' }}
+          repeat={Infinity}
+        />
+          
         </h1>
+        
 
         <p className={styles.description}>
           Get started by editing <code>pages/index.js</code>
