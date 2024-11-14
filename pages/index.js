42666859ee236b6f9f8e755023851a0c1ca5983f
Saisha <87923617+saisha-l@users.noreply.github.com>
2024-11-14 12:31:23 -0800

    added popoups

--- pages/index.js
+++ pages/index.js
@@ -2,105 +2,125 @@ import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import React from 'react';
 import { TypeAnimation } from 'react-type-animation';
+import Popup from 'reactjs-popup';
+
 export default function Home() {
   return (
     <div className={styles.grid}>
       <Head>
-      
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className={styles.title}>
-        <TypeAnimation
-          sequence={['Saisha Lakkoju.']}
-          wrapper="div"
-          cursor={false}
-          style={{ fontSize: '3em' }}
-          repeat={0}
-        />
-      
+          <TypeAnimation
+            sequence={['Saisha Lakkoju.']}
+            wrapper="div"
+            cursor={false}
+            style={{ fontSize: '3em' }}
+            repeat={0}
+          />
         </h1>
-        <div className={styles.rowContainer} >
-        <text className={styles.centered1}> 
-        Computer Science Student 
-        </text>
-        <img className={styles.img1} src={'./uwlogo2.png'}/>
-        <text className={styles.centered1}>
-        University of Washington
-        </text>
+
+        <div className={styles.rowContainer}>
+          <text className={styles.centered1}> 
+            Computer Science Student @ 
+          </text>
+          <img className={styles.img1} src={'./uwlogo2.png'}/>
+          <text className={styles.centered1}>
+            University of Washington
+          </text>
         </div>
+
         <text className={styles.centered}> 
-        Researcher and analyst with a passion for applying data science and technology to solve real world problems across healthcare and business domains. 
+          Researcher and analyst with a passion for applying data science and technology to solve real world problems across healthcare and business domains. 
         </text>
 
-        <h1 className={styles.centered2}> Experience </h1>
-
+        <h1 className={styles.centered2}>Experience</h1>
         <div className={styles.col1}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Market & Data Analyst, Business Impact Group &rarr;</h3>
-            <p>Accelerating growth for SMBs in the Seattle area through delivering data-driven insights. Strengthening stakeholder relationships, by presenting research in an accessible format using data visualization to present strategies to increase capital.</p>
-          </a>
+          <Popup trigger={<div className={styles.card}><h3>Market & Data Analyst, Business Impact Group &rarr;</h3></div>} modal>
+            {close => (
+              <div>
+                <h3>Market & Data Analyst, Business Impact Group</h3>
+                <p>Accelerating growth for SMBs in the Seattle area through delivering data-driven insights. Strengthening stakeholder relationships, by presenting research in an accessible format using data visualization to present strategies to increase capital.</p>
+                <button onClick={close} className={styles.closeButton}>Close</button>
+              </div>
+            )}
+          </Popup>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Research Intern, Fred Hutchinson Cancer Center &rarr;</h3>
-            <p>Currently working under the director of the Human Biology department, Dr. Eric Holland. Utilizing R and ML to analyze RNA-Seq data to understand molecular basis for different types of cancers.</p>
-          </a>
+          <Popup trigger={<div className={styles.card}><h3>Research Intern, Fred Hutchinson Cancer Center &rarr;</h3></div>} modal>
+            {close => (
+              <div>
+                <h3>Research Intern, Fred Hutchinson Cancer Center</h3>
+                <p>Currently working under the director of the Human Biology department, Dr. Eric Holland. Utilizing R and ML to analyze RNA-Seq data to understand molecular basis for different types of cancers.</p>
+                <button onClick={close} className={styles.closeButton}>Close</button>
+              </div>
+            )}
+          </Popup>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Research Intern, University of Washington Medicine &rarr;</h3>
-            <p>
-        Analyzing paralysis and lifespan assays on C. Elegans nematode worm for pre-clinical screening of therapeutic agents  for age-related diseases such as Parkinsons and Alzheimers.
-            </p>
+          <Popup trigger={<div className={styles.card}><h3>Research Intern, University of Washington Medicine &rarr;</h3></div>} modal>
+            {close => (
+              <div>
+                <h3>Research Intern, University of Washington Medicine</h3>
+                <p>Analyzing paralysis and lifespan assays on C. Elegans nematode worm for pre-clinical screening of therapeutic agents for age-related diseases such as Parkinsons and Alzheimers.</p>
+                <button onClick={close} className={styles.closeButton}>Close</button>
+              </div>
+            )}
+          </Popup>
 
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <Popup trigger={<div className={styles.card}><h3>Deploy &rarr;</h3></div>} modal>
+            {close => (
+              <div>
+                <h3>Deploy</h3>
+                <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
+                <button onClick={close} className={styles.closeButton}>Close</button>
+              </div>
+            )}
+          </Popup>
         </div>
 
-        <h1> Projects </h1>
-
+        <h1>Projects</h1>
         <div className={styles.col1}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          <Popup trigger={<div className={styles.card}><h3>Documentation &rarr;</h3></div>} modal>
+            {close => (
+              <div>
+                <h3>Documentation</h3>
+                <p>Find in-depth information about Next.js features and API.</p>
+                <button onClick={close} className={styles.closeButton}>Close</button>
+              </div>
+            )}
+          </Popup>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+          <Popup trigger={<div className={styles.card}><h3>Learn &rarr;</h3></div>} modal>
+            {close => (
+              <div>
+                <h3>Learn</h3>
+                <p>Learn about Next.js in an interactive course with quizzes!</p>
+                <button onClick={close} className={styles.closeButton}>Close</button>
+              </div>
+            )}
+          </Popup>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+          <Popup trigger={<div className={styles.card}><h3>Examples &rarr;</h3></div>} modal>
+            {close => (
+              <div>
+                <h3>Examples</h3>
+                <p>Discover and deploy boilerplate example Next.js projects.</p>
+                <button onClick={close} className={styles.closeButton}>Close</button>
+              </div>
+            )}
+          </Popup>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <Popup trigger={<div className={styles.card}><h3>Deploy &rarr;</h3></div>} modal>
+            {close => (
+              <div>
+                <h3>Deploy</h3>
+                <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
+                <button onClick={close} className={styles.closeButton}>Close</button>
+              </div>
+            )}
+          </Popup>
         </div>
-
       </main>
 
       <footer>
