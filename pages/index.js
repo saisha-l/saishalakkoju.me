99811f9b98c97ea57b3caec4cbc9c4419ec8a59c
Saisha <87923617+saisha-l@users.noreply.github.com>
2024-11-14 17:10:03 -0800

    added extra features + fonts

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,17 @@ import styles from '../styles/Home.module.css';
 import React from 'react';
 import { TypeAnimation } from 'react-type-animation';
 import Popup from 'reactjs-popup';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
   return (
     <div className={styles.grid}>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        
+        <link rel="icon" href="/sl.ico" />
+      
+
+
       </Head>
 
       <main>
@@ -18,6 +23,7 @@ export default function Home() {
             wrapper="div"
             cursor={false}
             style={{ fontSize: '3em' }}
+            className={styles.name}
             repeat={0}
           />
         </h1>
@@ -119,6 +125,47 @@ export default function Home() {
           cursor: pointer;
           border-radius: 4px;
           margin-top: 10px;
+        }
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+        footer {
+          width: 100%;
+          height: 100px;
+          border-top: 1px solid #eaeaea;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+        footer img {
+          margin-left: 0.5rem;
+        }
+        footer a {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          text-decoration: none;
+          color: inherit;
+        }
+        code {
+          background: #fafafa;
+          border-radius: 5px;
+          padding: 0.75rem;
+          font-size: 1.1rem;
+          font-family:
+            Menlo,
+            Monaco,
+            Lucida Console,
+            Liberation Mono,
+            DejaVu Sans Mono,
+            Bitstream Vera Sans Mono,
+            Courier New,
+            monospace;
         }
       `}</style>
 
